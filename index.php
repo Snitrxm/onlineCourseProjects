08e78cdf6f24c04bec7bb83103cd3847a13176b5
Snitrxm <andre.oliveira.roch@hotmail.com>
2022-05-21 17:25:39 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Página Inicial </title>
+</head>
+<body>
+    <nav id = "navegacao">    
+       <ul>
+           <li><a href = "index.php">Pagina Inicial</a></li>
+           <li><a href = "src/frontEnd/cursos.phtml">Cursos</a></li>
+           <li><a href = "src/frontEnd/sobre.phtml">Sobre</a></li>
+           <li><a href = "src/frontEnd/.phtml">Login</a></li>
+           <li><a href = "src/frontEnd/.phtml">Cadastro</a></li>
+       </ul>   
+    </nav>
+    <div id = "content">
+        <div id = "contentHeader">
+            <img src = "">
+        </div>
+    </div>
+</body>
+</html>
